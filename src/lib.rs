29beb852447aa1368ea5802fee3fd2a6ded36a93
Antti Lamminsalo <anttilamminsalo@admins-MacBook-Pro.local>
2017-04-27 14:11:47 +0300

    ok

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,38 @@ mod tests {
     fn it_works() {
     }
 }
+
+pub struct Node {
+    nums: Vec<u8>,
+    children: Vec<Node>
+}
+
+impl Node {
+    pub fn new(nums: Vec<u8>) -> Node {
+        Node{nums: nums, children: vec![]}
+    }
+
+    pub fn insert(&mut self, num: u8) {
+        if self.nums[self.nums.len()-1] < num {
+
+		let mut v = vec![];
+		v.extend(&self.nums);
+		v.push(num);
+            self.children.push(Node::new(v));
+
+            for mut node in self.children.iter_mut() {
+                node.insert(num);
+            }
+        }
+    }
+
+    pub fn extract(&self) -> Vec<Vec<u8>> {
+		let mut v: Vec<Vec<u8>> = vec![];
+		v.push(self.nums.clone());
+		for node in self.children.iter() {
+			v.extend(node.extract());
+		}
+		v
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,10 @@
-struct Node {
-    nums: Vec<u8>,
-    children: Vec<Node>
-}
-
-impl Node {
-    pub fn new(nums: Vec<u8>) -> Node {
-        Node{nums: nums, children: vec![]}
-    }
-
-    pub fn insert(&mut self, num: u8) {
-        if self.nums[self.nums.len()-1] < num {
-
-		let mut v = vec![];
-		v.extend(&self.nums);
-		v.push(num);
-            self.children.push(Node::new(v));
-
-            for mut node in self.children.iter_mut() {
-                node.insert(num);
-            }
-        }
-    }
-
-    pub fn extract(&self) -> Vec<Vec<u8>> {
-		let mut v: Vec<Vec<u8>> = vec![];
-		v.push(self.nums.clone());
-		for node in self.children.iter() {
-			v.extend(node.extract());
-		}
-		v
-    }
-}
+extern crate ordered_permutation;
+use ordered_permutation as op;
 
 fn main() {
-    let mut node = Node::new(vec![0]);
+    let mut node = op::Node::new(vec![0]);
     
-    for i in 1..10 {
+    for i in 1..7 {
         node.insert(i);
     }
     
